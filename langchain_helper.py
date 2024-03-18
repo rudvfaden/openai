06ddf247766b15a8562a3142ddef805289dafe14
Rud Faden <rudfaden@gmail.com>
2024-03-18 12:54:24 +0000

    working

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,9 +1,11 @@
 import streamlit as st
-from langchain.llms import OpenAI
+from langchain_openai import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chains import SequentialChain
-
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -25,5 +27,22 @@ def generate_pet_name(animal_type, pet_color, openai_api_key):
     return response
 
 
+def langchain_agent():
+    llm = OpenAI(temperature=0.5)
+
+    tools = load_tools(['wikipedia', 'llm-math'], llm=llm)
+
+    agent = initialize_agent(
+        tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+    )
+
+    result = agent.run(
+        "What is the average age of a dog? "
+    )
+
+    print(result)
+
+
 if __name__ == "__main__":
-    print(generate_pet_name("Dog", "Black"))
+    # print(generate_pet_name("Dog", "Black"))
+    langchain_agent()
